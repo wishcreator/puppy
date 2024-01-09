48712b39a0a8458f1667135512e52a344420f5cb
wishcreator <wishcreator@gmail.com>
2024-01-09 13:55:08 +0200

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wishcreator/puppy
 
 go 1.21.5
+
+require github.com/wishcreator/dog v0.0.0-20240109114927-5b5caade2195 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/wishcreator/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
